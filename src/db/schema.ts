e020c94486a370c2131c5547d0c30668c6e0ef1d
sakshamwithweb <websaksham39@gmail.com>
2025-12-17 12:13:28 +0530

    found wasn't working, now must

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,16 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { exec } from "child_process"
 
+exec("bunx drizzle-kit push", (error, stdout, stderr) => {
+    if (error) {
+        console.log("Error:", error.message)
+        return
+    }
+    if (stderr) {
+        console.log("Stderr:", stderr)
+        return
+    }
+})
 export const names = sqliteTable("names", {
     id: integer("id").primaryKey({ autoIncrement: true }),
     name: text("name").notNull(),
